9a8982830fee9014f731f0a8cc92b8f5122d1df7
Muthulingam Arultharan <arul@asiapac.com.sg>
2024-04-23 09:25:11 +0800

    formatting

--- examples/sample/outputs.tf
+++ examples/sample/outputs.tf
@@ -1,12 +1,12 @@
 output "ec2-id" {
   description = "EC2 instance ID"
-  value = module.ec2.ec2_id
+  value       = module.ec2.ec2_id
 }
 output "ebs_id" {
   description = "EBS volume ID"
-  value = module.ec2.ebs_id
+  value       = module.ec2.ebs_id
 }
 output "eni_id" {
   description = "ENI ID"
-  value = module.ec2.eni_id
+  value       = module.ec2.eni_id
 }
